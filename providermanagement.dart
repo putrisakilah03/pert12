6b9eadd838d3542bfb1ce54608cdb34933f04410
yudi0576 <129965125+yudi0576@users.noreply.github.com>
2023-06-16 10:47:32 +0700

    Add files via upload

---
+++ providermanagement.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_mysql/providercounter.dart';
+
+class ProviderManagement extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) {
+        print("1. Provider widget build");
+        return ProviderCounter();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Provider Management"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Consumer<ProviderCounter>(
+                builder: (context, providerCounter, child) {
+                  print("2. Consumer dijalankan");
+                  return Text(
+                    '${providerCounter.counter}',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                },
+              ),
+              Consumer<ProviderCounter>(
+                builder: (context, providerCounter, child) => ElevatedButton(
+                    onPressed: () {
+                      providerCounter.increment();
+                    },
+                    child: Text("Tambah")),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
